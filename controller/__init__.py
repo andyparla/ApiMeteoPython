3caff7a940806f433f1ba3c57e8c444e64ebd34d
andyparla <andyparla@gmail.com>
2021-06-23 22:50:57 +0200

    cambio estructura proyecto

--- controller/__init__.py
+++ controller/__init__.py
@@ -1,5 +1,5 @@
-from flask import Flask
-from flask_restful import Api
+from flask import Flask, jsonify, request
+from flask_restful import Api, Resource
 from controller.MeteoController import HelloWorld
 
 app = Flask(__name__)
