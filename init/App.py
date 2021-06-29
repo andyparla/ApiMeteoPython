ed1c92bcf238f430c299b6a261b3d9ca4d6ef22f
ATSISTEMAS\apuerto <apuerto@atsistemas.com>
2021-06-29 18:06:22 +0200

    no message

--- init/App.py
+++ init/App.py
@@ -1,11 +1,7 @@
-from flask import Flask, jsonify, request
-from flask_restful import Api, Resource
-#from ApiTest.controller.Controller import EstacionMeteo
-from ..controller.Controller import meteo
+import Flask, Api, EstacionMeteo
 
 app = Flask(__name__)
 api = Api(app)
-app.register_blueprint(meteo)
 
 
 api.add_resource(EstacionMeteo, '/api/<nombre>')
